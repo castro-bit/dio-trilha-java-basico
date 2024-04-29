f3f40130838e598b9146bd998f946b340edb72a7
castro-bit <gabrielpc@outlook.com>
2024-04-29 19:38:12 -0300

    Terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+      double saldo = 25;
+      double valorSolicitado = 18;
+  
+      if (saldo >= valorSolicitado) {
+        saldo -= valorSolicitado;
+      } else {
+        System.out.println("Saldo insuficiente");
+      }
+  
+      System.out.println("Saldo atual: " + saldo); 
+  
+      saldo = 15;
+      valorSolicitado = 22;
+  
+      if (saldo >= valorSolicitado) {
+        saldo -= valorSolicitado;
+      } else {
+        System.out.println("Saldo insuficiente");
+      }
+  
+      System.out.println("Saldo atual: " + saldo);
     }
-}
+  }
+  
